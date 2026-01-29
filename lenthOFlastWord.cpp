22aec0537758ded227e31e6d1ac5c076137a3af4
Prajwal <pmwaghmare0001@gmail.com>
2026-01-30 00:02:13 +0530

    explain

--- lenthOFlastWord.cpp
+++ lenthOFlastWord.cpp
@@ -17,3 +17,9 @@ public:
     }
 };
 
+i starts at index 11 (space ' ').
+Loop 1: s[11] == ' ' is true. i becomes 10.
+Loop 1: s[10] == ' ' is false (it's 'd'). The loop terminates.
+Loop 2: s[10] != ' ' is true (it's 'd'). Counting begins for "World".
+Loop 2: Continues until i reaches index 5, which is a space.
+Loop 2: s[5] != ' ' is false. The loop terminates.
