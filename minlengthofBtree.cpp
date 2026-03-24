600ebda06d856331275607db775c18c5a3efb69b
Prajwal <pmwaghmare0001@gmail.com>
2026-03-24 23:36:59 +0530

    min lenght of binary tree

---
+++ minlengthofBtree.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        
+        if (root == nullptr) {
+            return 0;
+        }
+
+        
+        if (root->left == nullptr && root->right == nullptr) {
+            return 1;
+        }
+
+        
+        if (root->left == nullptr) {
+            return minDepth(root->right) + 1;
+        }
+        if (root->right == nullptr) {
+            return minDepth(root->left) + 1;
+        }
+
+        
+        return min(minDepth(root->left), minDepth(root->right)) + 1;
+    }
+};
