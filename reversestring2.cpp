166078fbaebee812d5baa36574c60cfa8143a2bc
Prajwal <pmwaghmare0001@gmail.com>
2026-04-06 23:55:08 +0530

    reverse string 2

---
+++ reversestring2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n = s.length();
+        
+        
+        for (int i = 0; i < n; i += 2 * k) {
+            
+            int end = min(i + k, n);
+            
+            
+            reverse(s.begin() + i, s.begin() + end);
+        }
+        
+        return s;
+    }
+};
