f249dd374e2a9c6fbee7daebc0ad52184af6eb63
Prajwal <pmwaghmare0001@gmail.com>
2026-04-13 23:54:59 +0530

    summary ranges

---
+++ summaryranges.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> result;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++) {
+            int start = nums[i];
+            
+            
+            
+            while (i + 1 < n && (long long)nums[i + 1] == (long long)nums[i] + 1) {
+                i++;
+            }
+            
+            
+            if (start != nums[i]) {
+                result.push_back(to_string(start) + "->" + to_string(nums[i]));
+            } else {
+                result.push_back(to_string(start));
+            }
+        }
+        
+        return result;
+    }
+};
