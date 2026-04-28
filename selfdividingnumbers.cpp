e6701245a6a18f87ed4ea4fb6e6b85c94474c394
Prajwal <pmwaghmare0001@gmail.com>
2026-04-28 23:48:03 +0530

    self dividing numbers

---
+++ selfdividingnumbers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> result;
+        
+        for (int i = left; i <= right; ++i) {
+            if (isSelfDividing(i)) {
+                result.push_back(i);
+            }
+        }
+        
+        return result;
+    }
+
+private:
+    bool isSelfDividing(int num) {
+        int temp = num;
+        while (temp > 0) {
+            int digit = temp % 10;
+             
+            
+            if (digit == 0 || num % digit != 0) {
+                return false;
+            }
+            temp /= 10;
+        }
+        return true;
+    }
+};
